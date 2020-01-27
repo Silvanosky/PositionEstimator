22b0e785c00b2cc1ba031ad36e9c4b9ed0971a90
Thomas Delecroix <thomas.delecroix@epita.fr>
2020-01-27 14:11:21 +0100

    python: add file argv

--- position/python/main.py
+++ position/python/main.py
@@ -1,7 +1,8 @@
 from graph import Graph
 from parser import Parser
+import sys
 
-Parser.Parse('test6.p', 'test6.gr')
-graph = Graph('test6.gr')
+Parser.Parse('test' + sys.argv[1] + '.p', 'test' + sys.argv[1]  + '.gr')
+graph = Graph('test' + sys.argv[1] + '.gr')
 graph.load()
 graph.show()
